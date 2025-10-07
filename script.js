a2ef9e86b20551ad16977a289603dc60c5e468e2
Nicolas <gomes160107@gmail.com>
2025-10-06 22:17:33 -0300

    Trabalhando a Lógica do Js, e fazendo teste de interação ao usar o scroll do mouse.

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
 //Criação de uma timeline com GSAP com animações sincronizadas com o scroll
 var tl =gsap.timeline({
-    scrollTrigger:{
+    scrollTrigger: {
         trigger: '.two',
         start:'0% 95%',
         end: '70% 50%',
-        scrub:true,
+        scrub: true,
         markers: true, //DEPURAÇÃO
     },
-})
+});
+
+tl.to(
+    '#fanta',
+    {
+        top: '120%', //MOVE O ELEMENTO COM ID FANTA PARA 120% DO TOPO
+        left: '0%',
+    },
+    'orange'
+); //NOMEANDO ESSE TRECHO DE ANIMAÇÃO COMO 'orange' PARA SINCRONIZAÇÃO
